109b0e1c4afb9b5d723f79df91417587f2171171
Opilane TTHK <opilane@tthk.ee>
2020-02-12 15:18:06 +0200

    node

---
+++ server.js
@@ -0,0 +1,11 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+//console.log(superheroes.all);
+//console.log(superheroes.random());
+
+/*supervillains.all.forEach(supervillain => {
+    console.log("My super villain is ", supervillain)
+});*/
+
+console.log(supervillains.all.length)
